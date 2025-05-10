836b47c284141889e53536e8847a345106a61b36
Trombecher <107646151+Trombecher@users.noreply.github.com>
2025-05-10 02:20:14 +0200

    Remove tests and remove Bun dev dep

---
+++ tests/paths.test.ts
@@ -0,0 +1,35 @@
+import {expect, test} from "vitest";
+import {Color} from "../src";
+import {TOLERANCE} from "./common";
+
+const N = 100;
+
+test("sRGB HSV <-> Oklch", () => {
+    for(let i = 0; i < N; i++) {
+        const h = Math.random();
+        const s = Math.random();
+        const v = Math.random();
+
+        const randomColor = Color.fromHSV(
+            h,
+            s,
+            v,
+            "sRGB"
+        );
+
+        const newColor = Color.fromLCH(
+            randomColor.pl("Ok"),
+            randomColor.pc("Ok"),
+            randomColor.ph("Ok"),
+            "Ok"
+        );
+
+        const d = Math.hypot(
+            newColor.h("sRGB") - h,
+            newColor.sv("sRGB") - s,
+            newColor.v("sRGB") - v
+        );
+
+        expect(d).toBeLessThan(TOLERANCE);
+    }
+})
