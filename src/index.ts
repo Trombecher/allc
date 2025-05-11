b07963b16fa80b2c77ead934e3c22727ef41ef54
Trombecher <107646151+Trombecher@users.noreply.github.com>
2025-05-11 04:31:56 +0200

    Document color space types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 export * from "./conversions";
 export * from "./multi";
 
+/**
+ * A color space that is applicable to RGB-like color models. These are:
+ *
+ * * Linear RGB
+ * * RGB
+ * * HSL
+ * * HSV
+ * * HSI
+ */
 export type RGBColorSpace = "sRGB" | "Adobe RGB" | "Display P3";
+
+/**
+ * A _perceptual_ color space. Applicable to LAB-like color models. These are:
+ *
+ * * LAB
+ * * LCH
+ */
 export type PerceptualColorSpace = "Ok" | "CIE";
